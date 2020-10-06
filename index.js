8ea1bcf05c45e0b232a27f92b53084fab96b07b4
Zachary Long <42480238+zacharylong@users.noreply.github.com>
2020-10-06 15:06:15 -0400

    Chapter 4 end

--- index.js
+++ index.js
@@ -5,11 +5,25 @@ const svg = d3.select('.canvas')
         .attr('width', 600)
         .attr('height', 600);
 
+//create  margins and dimensions
+const margin = {top: 20, right: 20, bottom: 100, left: 100};
+const graphWidth = 600 - margin.left - margin.right;
+const graphHeight = 600  - margin.top - margin.bottom;
+
+const graph = svg.append('g')
+    .attr('width', graphWidth)
+    .attr('height', graphHeight)
+    .attr('transform', `translate(${margin.left},${margin.top})`)
+
+const xAxisGroup = graph.append('g')
+    .attr('transform', `translate(0, ${graphHeight})`);
+const yAxisGroup  = graph.append('g');
+
 d3.json('menu.json').then(data => {
 
     const y = d3.scaleLinear()
         .domain([0, d3.max(data, d => d.orders)])
-        .range([0, 500]);
+        .range([graphHeight, 0]);
 
     // const min = d3.min(data, d => d.orders);
     // const max = d3.max(data, d => d.orders);
@@ -23,19 +37,36 @@ d3.json('menu.json').then(data => {
 
     // join the  data to rects
 
-    const rects = svg.selectAll('rect')
+    const rects = graph.selectAll('rect')
         .data(data)
 
     rects.attr('width', x.bandwidth)
-        .attr('height', d => y(d.orders))
+        .attr('height', d => graphHeight - y(d.orders))
         .attr('fill', 'orange')
-        .attr('x', d => x(d.name));
+        .attr('x', d => x(d.name))
+        .attr('y', d => y(d.orders));
 
     // append  the enter selection to the DOM
     rects.enter()
         .append('rect')
         .attr('width', x.bandwidth)
-        .attr('height', d => y(d.orders))
+        .attr('height', d => graphHeight - y(d.orders))
         .attr('fill', 'orange')
-        .attr('x', d => x(d.name));
+        .attr('x', d => x(d.name))
+        .attr('y', d => y(d.orders));
+
+    //create and  call axes
+    const xAxis = d3.axisBottom(x);
+    const yAxis = d3.axisLeft(y)
+        .ticks(3)
+        .tickFormat(d => d + ' orders');
+
+    xAxisGroup.call(xAxis);
+    yAxisGroup.call(yAxis);
+
+    xAxisGroup.selectAll('text')
+        .attr('transform', 'rotate(-40)')
+        .attr('text-anchor', 'end');
+
+
 })
