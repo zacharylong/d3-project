0c23eac95c29dc32389a061207bccbf75fea3d04
Zachary Long <42480238+zacharylong@users.noreply.github.com>
2020-11-04 16:54:14 -0500

    add lines, sort data array after filtering based on button

---
+++ Project2 Fitness Tracker/index.js
@@ -0,0 +1,49 @@
+// DOM elements
+const btns = document.querySelectorAll('button');
+const form = document.querySelector('form');
+const formAct = document.querySelector('form span');
+const input = document.querySelector('input');
+const error = document.querySelector('.error');
+
+var activity ='cycling';
+
+btns.forEach(btn => {
+    btn.addEventListener('click', e => {
+        // get activity
+        activity = e.target.dataset.activity;
+
+        // remove and add active class
+        btns.forEach(btn => btn.classList.remove('active'));
+        e.target.classList.add('active');
+
+        // set id of input field
+        input.setAttribute('id', activity);
+
+        // set text of form span
+        formAct.textContent = activity;
+
+        // call update function
+        update(data);
+
+    })
+});
+
+// form submit
+form.addEventListener('submit', e => {
+    // prevent default action
+    e.preventDefault();
+
+    const distance =  parseInt(input.value);
+    if(distance){
+        db.collection('activities').add({
+            distance,
+            activity,
+            date: new Date().toString()
+        }).then(() => {
+            error.textContent = '';
+            input.value = '';
+        })
+    } else {
+        error.textContent =  'Please enter a valid distance';
+    }
+});
